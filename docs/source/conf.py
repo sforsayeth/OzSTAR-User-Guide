cde7a451d6b7a11f9b3213279b10f4439cd63785
macrocosme <danyvohl@hotmail.com>
2018-06-08 11:03:39 +1000

    Fix -- issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -118,7 +118,7 @@ html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'OzSTARUserGuidedoc'
-
+html_use_smartypants = False
 
 # -- Options for LaTeX output ---------------------------------------------
 
